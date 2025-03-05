cdbd9e11312b341ae23f7b39a97e03c5e26cd0e5
Kalel <ZoroPlay1220@gmail.com>
2025-03-05 16:18:58 -0300

    inicialize project

---
+++ src/app/components/login/login.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { InputComponent } from "../shared/input/input.component";
+
+@Component({
+  selector: 'app-login',
+  imports: [InputComponent],
+  templateUrl: './login.component.html',
+  styleUrl: './login.component.scss'
+})
+export class LoginComponent {
+
+}
